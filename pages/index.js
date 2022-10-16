33d498ada840a1338b77b5228c11d5417798294c
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-16 13:35:23 -0400

    feat(HomePage): styled background

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Text from '../components/Text/text'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-<Text value="Teste de texto 1" color="#52FB02" size="64" weight="bold" spacing='10'/>
+    <div className={styles.container}>
+      <div className={styles.bgImage}></div>
     </div>
   )
 }
