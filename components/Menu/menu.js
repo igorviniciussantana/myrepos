15f78f3433304b92bb0d8693f99f653283d5af93
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-16 11:44:08 -0400

    feat: created components files

---
+++ components/Menu/menu.js
@@ -0,0 +1,8 @@
+export default function Menu() {
+    return (
+      <div>
+  
+      </div>
+    )
+  }
+  
---
+++ components/Text/text.js
@@ -0,0 +1,7 @@
+export default function Text() {
+  return (
+    <div>
+
+    </div>
+  )
+}
