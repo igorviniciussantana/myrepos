d0a51dad652cbad240f432ae3674e2595d7146ce
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-16 17:16:51 -0400

    feat: configured homepage head

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Text from '../components/Text/text'
-import Image from 'next/image'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Text from "../components/Text/text";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.bgImage}></div>
-    </div>
-  )
+    <>
+      <Head>
+        <title>MyRepos | Página Inicial</title>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/images/favicon.svg" type="image/svg" />
+        <meta
+          name="description"
+          content="Veja seus repositórios no GitHub de uma forma mais bonita e interativa!"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.bgImage}></div>
+      </div>
+    </>
+  );
 }
