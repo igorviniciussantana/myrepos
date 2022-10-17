314c12f3f12e3b303b5c75cbdf9c0318d5a650d9
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-16 20:06:33 -0400

    feat: added useState and useEffect hooks

--- pages/users/[user].js
+++ pages/users/[user].js
@@ -1,16 +1,26 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function User() {
+  const router = useRouter();
+  const  userName  = router.query.user;
+
+  const [user, setUser] = useState({ userName });
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userName}`)
+      .then((userResponse) => userResponse.json())
+      .then((userData) => setUser(userData));
+  });
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userName}/repos`)
+      .then((reposResponse) => reposResponse.json())
+      .then((reposData) => setRepos(reposData));
+  });
 
 
-import { useRouter } from "next/router"
-
-export default function User(){
-
-const router = useRouter();
-const path = router.query
-return(
-
-    <h1>{path.user}</h1>
-)
-
-
-
+  return (<h1>{user.login}</h1>);
 }
