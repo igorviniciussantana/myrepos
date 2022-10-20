d752801e1f66295f018a5db6351d3fa67e361480
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-20 09:13:46 -0400

    style(button): indented button.js

--- components/Button/button.js
+++ components/Button/button.js
@@ -3,15 +3,14 @@ import Text from "../Text/text";
 import { useRouter } from "next/router";
 
 export default function Button(props) {
+  const router = useRouter();
 
-  const router = useRouter()
-
-    return (
-     <button className={styles.button} onClick={() => router.push(`users/${props.click}`)}>
-
-
-      <Text value={props.value} color="#100F11" size="1" weight="700"/>
-     </button>
-    )
-  }
-  
+  return (
+    <button
+      className={styles.button}
+      onClick={() => router.push(`users/${props.click}`)}
+    >
+      <Text value={props.value} color="#100F11" size="1" weight="700" />
+    </button>
+  );
+}
