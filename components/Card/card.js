08e39bb1acc9fd39baaa6f15b71fba6bb91a98c4
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-20 08:08:10 -0400

    feat(card): added card decoration

--- components/Card/card.js
+++ components/Card/card.js
@@ -2,36 +2,47 @@ import styles from "./card.module.css";
 import Text from "../Text/text";
 import Link from "next/link";
 
-
 export default function Card(props) {
-
-
-    return (
-      
-        <Link href={props.link}>
-<a className={styles.card}>
-
-<Text value={props.title} color="#100f11" weight="700" minSize="2" mediumSize="2"/>
-<Text value={props.description} color="#100f11" weight="400" minSize="1.5" mediumSize="1.5"/>
-  <Link href={`https://github.com/${props.login}`}>
-<a className={styles.user} target="_blank">
-  
-    <div className={styles.avatar}>
-        <img src={props.img} />
-    </div>
-    <Text value={"@" + props.login} weight="700" color="#100f11"/>
-  
-</a>
-  </Link>
-  <div className={styles.tagsDiv}>
-{props.tags.map(tag =>{
-    return(
-        <span className={styles.tagElement} key={tag}>{tag}</span>
-    )
-})}
-</div>
-</a>
-</Link>
-    )
-  }
-  
+  return (
+    <Link href={props.link}>
+      <a className={styles.card}>
+        <div className={styles.info}>
+        <Text
+          value={props.title}
+          color="#100f11"
+          weight="700"
+          minSize="2"
+          mediumSize="2"
+        />
+        <Text
+          value={props.description}
+          color="#100f11"
+          weight="400"
+          minSize="1.5"
+          mediumSize="1.5"
+        />
+        <Link href={`https://github.com/${props.login}`}>
+          <a className={styles.user} target="_blank">
+            <div className={styles.avatar}>
+              <img src={props.img} />
+            </div>
+            <Text value={"@" + props.login} weight="700" color="#100f11" />
+          </a>
+        </Link>
+        <div className={styles.tagsDiv}>
+          {props.tags.map((tag) => {
+            return (
+              <span className={styles.tagElement} key={tag}>
+                {tag}
+              </span>
+            );
+          })}
+        </div>
+        </div>
+       
+            <img src="/images/card_decorator.svg" className={styles.decoration}/>
+     
+      </a>
+    </Link>
+  );
+}
