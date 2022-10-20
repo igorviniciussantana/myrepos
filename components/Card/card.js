340258cdd020a6831c6d715f6d1ac156103d3dfa
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-20 08:20:26 -0400

    fix(card): changed decoration to background

--- components/Card/card.js
+++ components/Card/card.js
@@ -6,19 +6,18 @@ export default function Card(props) {
   return (
     <Link href={props.link}>
       <a className={styles.card}>
-        <div className={styles.info}>
         <Text
           value={props.title}
           color="#100f11"
           weight="700"
-          minSize="2"
+          minSize="1.4"
           mediumSize="2"
         />
         <Text
           value={props.description}
           color="#100f11"
           weight="400"
-          minSize="1.5"
+          minSize="0.8"
           mediumSize="1.5"
         />
         <Link href={`https://github.com/${props.login}`}>
@@ -26,7 +25,8 @@ export default function Card(props) {
             <div className={styles.avatar}>
               <img src={props.img} />
             </div>
-            <Text value={"@" + props.login} weight="700" color="#100f11" />
+            <Text value={"@" + props.login} weight="700" color="#100f11"  minSize="0.6"
+          mediumSize="0.9"/>
           </a>
         </Link>
         <div className={styles.tagsDiv}>
@@ -38,9 +38,9 @@ export default function Card(props) {
             );
           })}
         </div>
-        </div>
+    
        
-            <img src="/images/card_decorator.svg" className={styles.decoration}/>
+            {/* <img src="/images/card_decorator.svg" className={styles.decoration}/> */}
      
       </a>
     </Link>
