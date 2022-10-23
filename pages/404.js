f2c08b0d5f59c9d822094589f26bf874cc3f9e60
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-23 11:22:46 -0400

    feat(error): added favicon

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,19 @@
 import styles from "../styles/Error.module.css";
 import Text from "../components/Text/text";
+import Head from "next/head";
 
 export default function Error() {
 
 
   return (
+    <>
+    <Head>
+        <title>Error 404 | MyRepos</title>
+        <link rel="icon" href="/images/favicon.svg" type="image/svg" />
+    </Head>
     <div className={styles.container}>
     <Text value="Error" color="white"/> <Text value="404" color="white"/>
     </div>
+    </>
   );
 }
