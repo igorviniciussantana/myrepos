9b98d8a21bb3bdccdd36379666d02d3de65444ff
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-23 11:43:28 -0400

    feat(error): added page components

--- pages/404.js
+++ pages/404.js
@@ -1,19 +1,47 @@
 import styles from "../styles/Error.module.css";
 import Text from "../components/Text/text";
 import Head from "next/head";
+import Menu from "../components/Menu/menu";
+import Button from "../components/Button/button";
 
 export default function Error() {
-
-
   return (
     <>
-    <Head>
+      <Head>
         <title>Error 404 | MyRepos</title>
         <link rel="icon" href="/images/favicon.svg" type="image/svg" />
-    </Head>
-    <div className={styles.container}>
-    <Text value="Error" color="white"/> <Text value="404" color="white"/>
-    </div>
+      </Head>
+      <div className={styles.container}>
+        <Menu />
+        <div className={styles.app}>
+          <img src="/images/error.svg" className={styles.img} />
+          <div className={styles.title}>
+            <Text
+              value="error"
+              color="white"
+              weight="700"
+              minSize="2"
+              mediumSize="6"
+            />
+            <Text
+              value="404"
+              color="#52fb02"
+              weight="700"
+              minSize="2"
+              mediumSize="6"
+            />
+          </div>
+          <Text
+            value="Página não encontrada, verifique e tente novamente."
+            color="#646464"
+            weight="400"
+            minSize="1.3"
+            align="center"
+            mediumSize="1"
+          />
+          <Button value="Voltar para a Página Inicial"/>
+        </div>
+      </div>
     </>
   );
 }
