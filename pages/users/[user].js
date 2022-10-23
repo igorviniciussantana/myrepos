dd2c30c63a538247d5376ce4f4048e4634333616
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-23 14:27:43 -0400

    feat(user): added react-loading-skeleton dependency

--- pages/users/[user].js
+++ pages/users/[user].js
@@ -26,37 +26,47 @@ export default function User() {
       .then((reposData) => setRepos(reposData));
   }, [{ userName }]);
 
-  return (
-    <>
-      <Head>
-        <title>{user.login} | MyRepos</title>
-        <link rel="icon" href="/images/favicon.svg" type="image/svg" />
-        <meta
-          name="description"
-          content={
-            "Conheça os repositórios de " + user.login + " " + "no GitHub!"
-          }
-        />
-      </Head>
-      <div className={styles.containerDynamic}>
-        <Menu />
-        <Header avatar={user.avatar_url} name={user.name} login={user.login} />
-        <div className={styles.cardsDiv}>
-          {repos.map((repo) => {
-            return (
-              <Card
-                title={repo.name}
-                key={repo.id}
-                description={repo.description}
-                img={repo.owner.avatar_url}
-                login={repo.owner.login}
-                tags={repo.topics.slice(0, 3)}
-                link={repo.html_url}
-              />
-            );
-          })}
+console.log(user)
+
+
+
+    return (
+      <>
+        <Head>
+          <title>{user.login} | MyRepos</title>
+          <link rel="icon" href="/images/favicon.svg" type="image/svg" />
+          <meta
+            name="description"
+            content={
+              "Conheça os repositórios de " + user.login + " " + "no GitHub!"
+            }
+          />
+        </Head>
+        <div className={styles.containerDynamic}>
+          <Menu />
+          <Header
+            avatar={user.avatar_url}
+            name={user.name}
+            login={user.login}
+          />
+          <div className={styles.cardsDiv}>
+            {repos.map((repo) => {
+              return (
+                <Card
+                  title={repo.name}
+                  key={repo.id}
+                  description={repo.description}
+                  img={repo.owner.avatar_url}
+                  login={repo.owner.login}
+                  tags={repo.topics.slice(0, 3)}
+                  link={repo.html_url}
+                />
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  
+  
 }
