5b4eaa6f033e0f737d7532a0c5fd19b145f63d19
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-10 11:28:37 -0400

    feat(user): add getStaticProps

--- pages/users/[user].js
+++ pages/users/[user].js
@@ -26,47 +26,56 @@ export default function User() {
       .then((reposData) => setRepos(reposData));
   }, [{ userName }]);
 
-console.log(user)
+  console.log(user);
 
-
-
-    return (
-      <>
-        <Head>
-          <title>{user.login} | MyRepos</title>
-          <link rel="icon" href="/images/favicon.svg" type="image/svg" />
-          <meta
-            name="description"
-            content={
-              "Conheça os repositórios de " + user.login + " " + "no GitHub!"
-            }
-          />
-        </Head>
-        <div className={styles.containerDynamic}>
-          <Menu />
-          <Header
-            avatar={user.avatar_url}
-            name={user.name}
-            login={user.login}
-          />
-          <div className={styles.cardsDiv}>
-            {repos.map((repo) => {
-              return (
-                <Card
-                  title={repo.name}
-                  key={repo.id}
-                  description={repo.description}
-                  img={repo.owner.avatar_url}
-                  login={repo.owner.login}
-                  tags={repo.topics.slice(0, 3)}
-                  link={repo.html_url}
-                />
-              );
-            })}
-          </div>
+  return (
+    <>
+      <Head>
+        <title>{user.login} | MyRepos</title>
+        <link rel="icon" href="/images/favicon.svg" type="image/svg" />
+        <meta
+          name="description"
+          content={
+            "Conheça os repositórios de " + user.login + " " + "no GitHub!"
+          }
+        />
+      </Head>
+      <div className={styles.containerDynamic}>
+        <Menu />
+        <Header avatar={user.avatar_url} name={user.name} login={user.login} />
+        <div className={styles.cardsDiv}>
+          {repos.map((repo) => {
+            return (
+              <Card
+                title={repo.name}
+                key={repo.id}
+                description={repo.description}
+                img={repo.owner.avatar_url}
+                login={repo.owner.login}
+                tags={repo.topics.slice(0, 3)}
+                link={repo.html_url}
+              />
+            );
+          })}
         </div>
-      </>
-    );
-  
-  
+      </div>
+    </>
+  );
 }
+
+export const getStaticProps = async function (context) {
+  const { userName } = context.params;
+
+  const [userResponse, reposResponse] = Promise.all([
+    api.get(`/users/${userName}`),
+    api.get(`/users/${userName}/repos`),
+  ]);
+
+  return {
+    props: {
+      user: userResponse.data,
+      repos: reposResponse.data,
+    },
+    revalidate: 20,
+  };
+};
