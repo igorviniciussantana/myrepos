aaa79df472e1b02414b4b2ed85e91cb8ffe8f11c
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-10 11:13:40 -0400

    feat: add api file

--- components/Header/header.js
+++ components/Header/header.js
@@ -2,6 +2,9 @@ import styles from "./header.module.css";
 import Link from "next/link";
 import Text from "../Text/text";
 
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 export default function Header(props) {
   return (
     <div className={styles.header}>
@@ -11,7 +14,7 @@ export default function Header(props) {
       <Link href={`https://github.com/${props.login}`}>
         <a target="_blank">
           <Text
-            value={props.name}
+            value={props.name || <Skeleton />}
             minSize="1.8"
             mediumSize="3"
             color="white"
@@ -23,7 +26,7 @@ export default function Header(props) {
       <Link href={`https://github.com/${props.login}`}>
         <a target="_blank">
           <Text
-            value={"@" + props.login}
+            value={("@" + props.login || <Skeleton />)}
             minSize="0.8"
             mediumSize="1.3"
             color="#646464"
