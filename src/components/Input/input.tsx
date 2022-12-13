040450b3d910989f17e5b7838136011e2898adb1
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-13 08:13:47 -0400

    refactor(input): add types

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,6 +1,11 @@
 import styles from "./input.module.css";
 
-export default function Input(props) {
+interface InputProps{
+  placeholder: string;
+  change: (e: string)=> void;
+}
+
+export default function Input(props : InputProps) {
     return (
     <input placeholder={props.placeholder} className={styles.input} onChange={e => props.change(e.target.value)}/>
     )
